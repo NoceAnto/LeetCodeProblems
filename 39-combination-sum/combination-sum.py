c7d9449843f908396489a24746832f9b2f4d5c52
Antonio Nocera <131775158+NoceAnto@users.noreply.github.com>
2026-09-09 21:30:58 +0200

    Time: 31 ms (6.29%) | Memory: 19.7 MB (10.01%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = []; 
+
+        candidates.sort(reverse=True)
+        #seen_candidates = set()
+
+        def backtrack(current):
+            #print(sum(current),current,target)
+            if (sum(current) == target): 
+                    output.append(current) 
+                    
+                    return None     
+            if sum(current)>target: 
+                return None
+
+            if sum(current)<target:
+                for digit in candidates: 
+                    if sum(current)==0:
+                        if target-sum(current)>=digit:
+                            
+                            backtrack(current+[digit])
+                    if sum(current)>0 and current[-1]>=digit:
+                        if target-sum(current)>=digit:
+                            
+                            backtrack(current+[digit])
+        
+        backtrack([])
+
+        
+        result = [];
+
+        #for i in range(0,len(output)): 
+        #    if sum(output[i])!=0:
+        #        result.append(output[i]) 
+
+
+        return output 
