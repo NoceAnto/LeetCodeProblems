7eca4096e9ea0c1da2279e77ce9fbad41e853a74
Antonio Nocera <131775158+NoceAnto@users.noreply.github.com>
2026-09-10 10:48:43 +0200

    Time: 0 ms (100.00%) | Memory: 20.5 MB (18.38%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,17 +2,17 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         two_sum_dict={}; 
-        seen = set(); 
+        #seen = set(); 
 
 
         for i in range(0,len(nums)): 
 
             needed = target-nums[i];
 
-            if needed in seen:
+            if needed in two_sum_dict.keys():
                 return [two_sum_dict[needed],i]
 
-            seen.add(nums[i]);
+            #seen.add(nums[i]);
             two_sum_dict[nums[i]] = i; 
-            
+
         
