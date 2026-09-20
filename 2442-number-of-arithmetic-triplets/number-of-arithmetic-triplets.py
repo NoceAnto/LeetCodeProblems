f1292fb9fe9568905267db0f323519cf8a259d30
Antonio Nocera <131775158+NoceAnto@users.noreply.github.com>
2026-09-20 11:09:08 +0200

    Time: 3 ms (59.13%) | Memory: 19.4 MB (20.10%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,18 +1,28 @@
 class Solution:
+    def binarysearch(self,nums: list[int], target: int) -> bool:
+        low,high = 0,len(nums)-1
+
+        while low<=high: 
+            
+            mid = low + (high-low)//2;
+            if nums[mid] == target: 
+                return True
+            
+            if nums[mid]<target: 
+                low = mid+1; 
+            else: 
+                high = mid-1; 
+
+        return False
+
+
     def arithmeticTriplets(self, nums: list[int], diff: int) -> int:
         
         output = 0; 
-        N = len(nums);
 
-        seen = set();
-        for i in range(0,N): 
-
-            lastnum = nums[i]; 
-            
-            if lastnum-diff in seen and lastnum-2*diff in seen: 
-                output = output +1; 
-
-            seen.add(lastnum)
+        for num in nums: 
+            if self.binarysearch(nums,num+diff) and self.binarysearch(nums,num+2*diff):
+                output +=1
 
 
         return output 
